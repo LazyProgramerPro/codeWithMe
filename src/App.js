549183dc1f503ab1c8e53ccb8eb390d284bb3dc6
[BK] Anh Túc <husthunter97@gmail.com>
2023-02-11 11:38:58 +0700

    [test] git test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello Thường</h1>
+      <h1> Hello Thường + Đức Anh</h1>
     </div>
   );
 }
