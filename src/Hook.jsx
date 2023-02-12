5d8846341becb2b96672c2efad82e73552f95c5e
[BK] True Blues <anhdd.101197@gmail.com>
2023-02-12 08:57:03 +0700

    i init hook example

---
+++ src/Hook.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './Hook.css'
+export default Example;
+
+function Example() {
+  // Khai báo 1 biến trạng thái mới đặt tên là "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Bạn đã click {count} lần</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click vào tôi
+      </button>
+    </div>
+  );
+}
+
